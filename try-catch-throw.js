a373c3a885d6f711b7aea072add3ab34131e5623
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-10-14 02:06:26 -0300

    Tratando e lançando erros

---
+++ try-catch-throw.js
@@ -0,0 +1,21 @@
+// Tratando e lançando erros
+
+try {
+  console.log(naoExisto)
+} catch (err) {
+  console.log('essa variavel não existe')
+}
+
+function soma(x, y) {
+  if (typeof x !== 'number' || typeof y !== 'number') {
+    throw new Error('x e y precisam ser números')
+  }
+  return x + y
+}
+
+try {
+  console.log(soma(5, 6))
+  console.log(soma('b', 8))
+} catch (err) {
+  //console.log(err)
+}
