7e629b287bded48f1d1e438a935ceb887e25f629
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-12-01 00:54:45 -0300

    Validação de CPF com classes

---
+++ OOP/cpf.js
@@ -0,0 +1,51 @@
+class ValidaCPF {
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfEnviado.replace(/\D+/g, '')
+    })
+  }
+
+  sequencia() {
+    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo
+  }
+
+  geraNovoCpf() {
+    const cpfSemDigitos = this.cpfLimpo.slice(0, -2)
+    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos)
+    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1)
+    this.novoCPF = cpfSemDigitos + digito1 + digito2
+  }
+
+  static geraDigito(cpfSemDigitos) {
+    let total = 0
+    let reverso = cpfSemDigitos.length + 1
+
+    for (let stringNum of cpfSemDigitos) {
+      total += reverso * Number(stringNum)
+      reverso--
+    }
+    const digito = 11 - (total % 11)
+    return digito <= 9 ? String(digito) : '0'
+  }
+
+  valida() {
+    if (!this.cpfLimpo) return false
+    if (typeof this.cpfLimpo !== 'string') return false
+    if (this.cpfLimpo.length !== 11) return false
+    if (this.sequencia()) return false
+    this.geraNovoCpf()
+
+    return this.novoCPF === this.cpfLimpo
+  }
+}
+
+const cpf = new ValidaCPF('070.987.720-03')
+
+if (cpf.valida()) {
+  console.log('CPF válido')
+} else {
+  console.log('CPF inválido')
+}
