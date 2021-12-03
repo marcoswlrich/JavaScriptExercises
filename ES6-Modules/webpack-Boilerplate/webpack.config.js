4cbc8631737c033a0ba40e47f44246af95d86000
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-12-03 02:11:11 -0300

    Webpack (Boilerplate)

---
+++ ES6-Modules/webpack-Boilerplate/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path') //CommonJS
+
+//config do webpack
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
