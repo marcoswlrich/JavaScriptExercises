1108dccbce5bf6d83821c5a2052b1a623d1b4067
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-12-02 23:16:37 -0300

    Fetch API (GET)

---
+++ JS-Assincrono/Fetch-API/assets/JS/main.js
@@ -0,0 +1,51 @@
+// const request = obj => {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open(obj.method, obj.url, true)
+//     xhr.send()
+
+//     xhr.addEventListener('load', () => {
+//       if (xhr.status >= 200 && xhr.status < 300) {
+//         resolve(xhr.responseText)
+//       } else {
+//         reject(xhr.statusText)
+//       }
+//     })
+//   })
+// }
+
+document.addEventListener('click', event => {
+  const element = event.target
+  const tag = element.tagName.toLowerCase()
+
+  if (tag === 'a') {
+    event.preventDefault()
+    carregaPagina(element)
+  }
+})
+
+async function carregaPagina(element) {
+  try {
+    const href = element.getAttribute('href')
+    const response = await fetch(href)
+    if (response.status !== 200) throw new Error('Erro 404!')
+
+    const html = await response.text()
+    carregaResultado(html)
+  } catch (error) {
+    console.log(error)
+  }
+
+  // fetch(href)
+  //   .then(response => {
+  //     if (response.status !== 200) throw new Error('Erro 404!')
+  //     return response.text()
+  //   })
+  //   .then(html => carregaResultado(html))
+  //   .catch(err => console.log(err))
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado')
+  resultado.innerHTML = response
+}
