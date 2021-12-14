d89cd69a804a0667b71a0280bf22d66829cdb66f
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-12-14 01:26:46 -0300

    Config MongoDB

--- Node/express-views/server.js
+++ Node/express-views/server.js
@@ -1,5 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    console.log('Conectado a base de dados')
+    app.emit('OK')
+  })
+  .catch(err => console.log(err))
+
 const routes = require('./routes')
 const path = require('path')
 const middlewares = require('./src/middlewares/middleware')
@@ -17,7 +28,9 @@ app.set('view engine', 'ejs')
 app.use(middlewares)
 app.use(routes)
 
-app.listen(3000, () => {
-  console.log('Acessar http://localhost:3000')
-  console.log('Servidor rodando...')
+app.on('OK', () => {
+  app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000')
+    console.log('Servidor rodando...')
+  })
 })
