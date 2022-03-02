c10ba41ca883b9bffa58fe343b645cbf07be8688
marcoswlrich <marcosapw.dev@gmail.com>
2022-03-01 22:10:59 -0300

    Exercicio com Promise

---
+++ Exercicios/ex1.js
@@ -0,0 +1,24 @@
+/*
+Em linguagens síncronas é comum a função sleep(tempo) assim, 
+pode-se por exemplo escrever algo no console, pausar o script 
+por x tempo e escrever algo mais algo como:
+  console.log('Olá!')
+  sleep(2000)
+  console.log('Depois de 2segs')
+  simule o sleep de maneira correta em Javascript (sem travar 
+  a thread principal) utilizando promises e uma função async/await
+
+*/
+
+const sleep = (tempo) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, tempo);
+  });
+
+const executar = async () => {
+  console.log("Olá!");
+  await sleep(2000);
+  console.log("Depois de 2segs");
+};
+
+executar();
